0dd60a7cda7e6ee3cd59436816af2f4dc9ec8b20
manas5996 <manas5996@gmail.com>
2023-02-22 18:13:49 +0530

    code commit!

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import redis from "redis";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
 dotenv.config();
 import rootRoute from "./routes/rootRoute.js";
-
 import connectDB from "./config/db.js";
 
 ///////////////////////////////////////start-mongodb-mongoose-connection-code/////////////////////////////////////////////////////////////////////////
@@ -13,29 +16,68 @@ connectDB();
 
 /// express server configuration
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+  },
+});
+
+// Create a Redis client
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
+
+redisClient.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redisClient.on("error", (err) => {
+  console.error(`Redis error: ${err}`);
+});
+
 app.use(express.json());
 // Body parser
 app.use(express.urlencoded({ extended: true }));
+
+// handle chat messages
+io.on("connection", (socket) => {
+  console.log(`Socket ${socket.id} connected`);
+
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    console.log(`Socket ${socket.id} joined room ${roomId}`);
+    redisClient.sadd(roomId, userId);
+  });
+
+  socket.on("leave-room", (roomId, userId) => {
+    socket.leave(roomId);
+    console.log(`Socket ${socket.id} left room ${roomId}`);
+    redisClient.srem(roomId, userId);
+  });
+
+  socket.on("send-message", (roomId, message, sender) => {
+    console.log(`Message received from ${sender}: ${message}`);
+    io.to(roomId).emit("receive-message", message, sender);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Socket ${socket.id} disconnected`);
+  });
+});
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 app.use("/", rootRoute);
-
 app.use(notFound);
 app.use(errorHandler);
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
+// start server
 const PORT = process.env.PORT || 5000;
-app.listen(
+httpServer.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
